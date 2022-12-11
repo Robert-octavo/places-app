6303713efaa46fdc7ce28ebf975663a423fb5b3d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-10 22:48:22 -0500

    folders

---
+++ frontend/src/user/pages/Users.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1', 
+      name: 'Max Schwarz', 
+      image: 'https://github.com/Robert-octavo/holbertonschool-web_front_end/blob/master/0x02-CSS_advanced/images/pic-person-03.jpg?raw=true', 
+      places: 3,
+    }
+  ]
+  // const USERS = []
+  return (
+    <UsersList items={USERS} />
+  )
+}
+
+export default Users
