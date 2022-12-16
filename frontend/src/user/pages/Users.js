08a0c198502902b56e7a5c93ffc68740c238ca5d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-15 22:08:01 -0500

    add another user to test

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -8,7 +8,13 @@ const Users = () => {
       name: 'Max Schwarz', 
       image: 'https://github.com/Robert-octavo/holbertonschool-web_front_end/blob/master/0x02-CSS_advanced/images/pic-person-03.jpg?raw=true', 
       places: 3,
-    }
+    },
+    {
+      id: 'u2', 
+      name: 'Lin Schwarz', 
+      image: 'https://github.com/Robert-octavo/holbertonschool-web_front_end/blob/master/0x02-CSS_advanced/images/pic-person-02.jpg?raw=true', 
+      places: 4,
+    },
   ]
   // const USERS = []
   return (
