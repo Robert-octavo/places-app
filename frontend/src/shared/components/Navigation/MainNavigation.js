ab4441332bbdb725f78e88919312092403908c11
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-16 20:55:52 -0500

    Drawer State

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,41 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  }
+
   return (
-    <MainHeader>
-      <button className='main-navigation__menu-btn'>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className='main-navigation__title'>
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav className='main-navigation__header-nav'>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      {draweIsOpen && (
+        <SideDrawer>
+          <nav className='main-navigation__drawer-nav'>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+        )}
+      <MainHeader>
+        <button className='main-navigation__menu-btn' onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   )
 }
 
