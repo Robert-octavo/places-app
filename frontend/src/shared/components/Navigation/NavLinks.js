0764de26e87d8248c10c049676a8a513701d1820
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-27 00:09:56 -0500

    Use authcontext to show some of the routes

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
 
 import './NavLinks.css';
 
 const NavLinks = props => {
+  const auth = useContext(AuthContext); // auth is an object with isLoggedIn, login, and logout properties (see frontend\src\shared\context\auth-context.js)
+
   return (
     <ul className='nav-links'>
       <li>
         <NavLink to="/" exact>ALL USERS</NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">Add Place</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">Authenticate</NavLink>
-      </li>
+      {auth.isLoggedIn && ( 
+        <li>
+          <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">Add Place</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LogOut</button>
+        </li>
+      )}
     </ul>
   )
 };
