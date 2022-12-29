9808260ec4f3da95f3cb10916804d71fcfc8f9de
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-28 23:24:35 -0500

    add update and delete place by id - add its routes

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,7 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   { 
     id: 'p1',
     title: 'Empire State Building',
@@ -43,16 +43,16 @@ const getPlaceById = (req, res, next) => {
 
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid; // { uid: 'u1' }
-  const place = DUMMY_PLACES.find(p => {
+  const places = DUMMY_PLACES.filter(p => {
     return p.creator === userId;
   });
 
-  if (!place) {
-    const error = 'Could not find a place for the provided user id.';
+  if (!places || places.length === 0) {
+    const error = 'Could not find places for the provided user id.';
     return next( new HttpError(error, 404));
   }
 
-  res.json({ Place: place }); // => { Place: { id: 'p1', title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world!', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg', address: '20 W 34th St, New York, NY 10001', location: { lat: 40.7484405, lng: -73.9878584 }, creator: 'u1' } }
+  res.json({ Places: places }); // => { Place: { id: 'p1', title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world!', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg', address: '20 W 34th St, New York, NY 10001', location: { lat: 40.7484405, lng: -73.9878584 }, creator: 'u1' } }
 };
 
 const createPlace = (req, res, next) => {
@@ -71,7 +71,25 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ Place: createdPlace }); // => { Place: { title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world!', location: { lat: 40.7484405, lng: -73.9878584 }, address: '20 W 34th St, New York, NY 10001', creator: 'u1' } }
 };
 
+const updatePlaceById = (req, res, next) => {
+    const { title, description } = req.body;
+    const placeId = req.params.pid;
+    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+    updatedPlace.title = title;
+    updatedPlace.description = description;
+    DUMMY_PLACES[placeIndex] = updatedPlace;
+    res.status(200).json({ Place: updatedPlace });
+};
+
+const deletePlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId); // Filter out the place with the id that matches the placeId param in the request url (pid) and reassign the DUMMY_PLACES array to the new array that is returned from the filter method (which is all the places that do not have the id that matches the placeId param in the request url (pid))
+  res.status(200).json({ message: 'Deleted place.' });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlaceById = updatePlaceById;
+exports.deletePlaceById = deletePlaceById;
--- backend/routes/places-route.js
+++ backend/routes/places-route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getPlaceById, getPlacesByUserId, createPlace } = require('../controllers/places-controllers');
+const { getPlaceById, getPlacesByUserId, createPlace, updatePlaceById, deletePlaceById } = require('../controllers/places-controllers');
 
 const router = express.Router(); // To register routes - can export this router to app.js
 
 router.get('/:pid', getPlaceById);
 router.get('/user/:uid', getPlacesByUserId);
 router.post('/', createPlace);
+router.patch('/:pid', updatePlaceById);
+router.delete('/:pid', deletePlaceById);
 
 module.exports = router; // Export this router to app.js
