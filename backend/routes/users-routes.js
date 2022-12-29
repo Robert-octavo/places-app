9adad34f3056013efab6e5aaad91f09d90bf39f0
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-29 00:06:23 -0500

    Add user routes and dummy users for test

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const userControllers = require('../controllers/users-controllers');
 
 const router = express.Router();
 
-router.get('/:userid', (req, res, next) => {
-    res.json({ message: 'It works!' });
-});
+
+router.get('/', userControllers.getUsers);
+router.post('/signup', userControllers.signup);
+router.post('/login', userControllers.login);
+// router.get('/login/:username/:password', userControllers.getUserByUsernameAndPassword);
 
 module.exports = router;
