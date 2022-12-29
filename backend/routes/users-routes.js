f3a8f3c6fe4e22d819a6ce7e3667bedb75b5ee53
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-29 00:38:59 -0500

    use validation result and check from express-validator to check fields

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const userControllers = require('../controllers/users-controllers');
+const { check } = require('express-validator');
 
 const router = express.Router();
 
 
 router.get('/', userControllers.getUsers);
-router.post('/signup', userControllers.signup);
+router.post(
+  '/signup',
+  [
+    check('name').notEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  userControllers.signup
+);
 router.post('/login', userControllers.login);
 // router.get('/login/:username/:password', userControllers.getUserByUsernameAndPassword);
 
