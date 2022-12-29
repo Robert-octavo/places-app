9644850915a25ef1b233bcbc347fa8f020bcbe36
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-29 00:39:52 -0500

    use cj=heck and validationresult from express-validator to check fields in the request

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError = require('../models/http-error');
+const { validationResult } = require('express-validator');
 
 let DUMMY_PLACES = [
   { 
@@ -56,6 +57,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuidv4(),
@@ -72,18 +78,29 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlaceById = (req, res, next) => {
-    const { title, description } = req.body;
-    const placeId = req.params.pid;
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
-    DUMMY_PLACES[placeIndex] = updatedPlace;
-    res.status(200).json({ Place: updatedPlace });
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
+
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+  res.status(200).json({ Place: updatedPlace });
 };
 
 const deletePlaceById = (req, res, next) => {
   const placeId = req.params.pid;
+  
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id.', 404);
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId); // Filter out the place with the id that matches the placeId param in the request url (pid) and reassign the DUMMY_PLACES array to the new array that is returned from the filter method (which is all the places that do not have the id that matches the placeId param in the request url (pid))
   res.status(200).json({ message: 'Deleted place.' });
 };
--- backend/routes/places-route.js
+++ backend/routes/places-route.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const { check } = require('express-validator');
 const { getPlaceById, getPlacesByUserId, createPlace, updatePlaceById, deletePlaceById } = require('../controllers/places-controllers');
 
 const router = express.Router(); // To register routes - can export this router to app.js
 
 router.get('/:pid', getPlaceById);
 router.get('/user/:uid', getPlacesByUserId);
-router.post('/', createPlace);
-router.patch('/:pid', updatePlaceById);
+router.post(
+  '/', 
+  [
+    check('title').notEmpty(), 
+    check('description').isLength({min: 5}),
+    check('address').notEmpty()
+  ], 
+    createPlace
+);
+router.patch(
+  '/:pid',
+  [
+    check('title').notEmpty(),
+    check('description').isLength({min: 5})
+  ],
+  updatePlaceById);
 router.delete('/:pid', deletePlaceById);
 
 module.exports = router; // Export this router to app.js
