533d506a0bff0ca530a7b8e664d75596d6693334
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-28 20:52:14 -0500

    Error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,15 @@ const app = express();
 app.use('/api/places', placesRoutes); /* Use places-route.js  just with /api/places/*/
 app.use('/api/users', usersRoutes); /* Use users-route.js  just with /api/users/*/
 
+// Error handling middleware
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+});S
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
--- backend/routes/places-route.js
+++ backend/routes/places-route.js
@@ -35,6 +35,13 @@ router.get('/:pid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ Place: place }); // => { Place: { id: 'p1', title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world!', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg', address: '20 W 34th St, New York, NY 10001', location: { lat: 40.7484405, lng: -73.9878584 }, creator: 'u1' } }
 });
 
@@ -43,6 +50,13 @@ router.get('/user/:uid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
+  
   res.json({ Place: place }); // => { Place: { id: 'p1', title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world!', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg', address: '20 W 34th St, New York, NY 10001', location: { lat: 40.7484405, lng: -73.9878584 }, creator: 'u1' } }
 });
 
