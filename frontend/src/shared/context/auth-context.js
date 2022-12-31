16eef4657db8f165cf2e63e19a179eb146ed8a26
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 13:33:00 -0500

    add the userId from the backend

--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -1,7 +1,8 @@
 import { createContext } from 'react';
 
 export const AuthContext = createContext({
-  isLoggedIn: false,
+  isLoggedIn: false, 
+  userId: null, // this is the user id from the backend
   login: () => {},
   logout: () => {}
 });
