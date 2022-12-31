ffa08b756b6a89890d4108ba07433a03fe3d8978
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-30 21:25:18 -0500

    add relation the places and users

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,8 +7,8 @@ const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
     image: { type: String, required: true },
-    places: { type: String, required: true}
-    // places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
+    // places: { type: String, required: true}
+    places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
 });
 
 userSchema.plugin(uniqueValidator); // This plugin will validate the unique property of the email field
