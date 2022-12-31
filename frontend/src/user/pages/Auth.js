6eff65a8222f4064b80e27ec30277a3bfc3aa9dc
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 13:32:27 -0500

    add the userid getting from response Data

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -60,7 +60,7 @@ const Auth = () => {
     // console.log(formState.inputs); // send this to the backend!
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/login',
           'POST',
           JSON.stringify({
@@ -71,11 +71,11 @@ const Auth = () => {
             'Content-Type': 'application/json'
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {} // the error is handled by the useHttpClient hook
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
           JSON.stringify({
@@ -87,7 +87,7 @@ const Auth = () => {
             'Content-Type': 'application/json'
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {} // the error is handled by the useHttpClient hook
     }
   };
