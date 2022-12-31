d799fe503c04d6b9a3cd3f2e69bae9678789f7be
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 11:41:03 -0500

    Display the  users in the DB

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
   const USERS = [
@@ -16,9 +18,45 @@ const Users = () => {
       places: 4,
     },
   ]
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
   // const USERS = []
+  // Use the fetch API to get the users from the backend server and store them in USERS
+  // UseEffect is a hook that runs after the component is rendered for the first time and after every update of the component (when the state changes) 
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <UsersList items={USERS} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
   )
 }
 
