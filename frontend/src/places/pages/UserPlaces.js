bd18d4cea9d26ba30b1ee3f19aee97be0fd1a082
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 14:23:42 -0500

    list the places

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,42 +1,65 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
-import PlaceList from '../components/PlaceList'
+import PlaceList from '../components/PlaceList';
 
-
-const DUMMY_PLACES = [
-  { 
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u2'
-  }
-]
+// const DUMMY_PLACES = [
+//   { 
+//     id: 'p1',
+//     title: 'Empire State Building',
+//     description: 'One of the most famous sky scrapers in the world!',
+//     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+//     address: '20 W 34th St, New York, NY 10001',
+//     location: {
+//       lat: 40.7484405,
+//       lng: -73.9878584
+//     },
+//     creator: 'u1'
+//   },
+//   {
+//     id: 'p2',
+//     title: 'Empire State Building',
+//     description: 'One of the most famous sky scrapers in the world!',
+//     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+//     address: '20 W 34th St, New York, NY 10001',
+//     location: {
+//       lat: 40.7484405,
+//       lng: -73.9878584
+//     },
+//     creator: 'u2'
+//   }
+// ]
 
 const UserPlaces = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
+  useEffect(() => { // this function will run after the component is rendered just once
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]); 
+
+  // const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
   return (
-    <PlaceList items={loadedPlaces} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
   )
 }
 
