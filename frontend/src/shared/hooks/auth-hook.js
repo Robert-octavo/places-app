27e54b448add55dea489986636b1cffbf04d1563
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 22:12:09 -0500

    Create a custom hook to manage the token, login, logout, and the userid outside of the App

---
+++ frontend/src/shared/hooks/auth-hook.js
@@ -0,0 +1,49 @@
+import React, { useState, useCallback, useEffect } from "react";
+
+let logoutTimer;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(false);
+  const [userId, setUserId] = useState(false); // this is the user id from the backend
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(); // this is the expiration date from the backend
+  
+  const login = useCallback((uid, token, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // 1 hour from now
+    setTokenExpirationDate(tokenExpirationDate); // this is the expiration date from the backend
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpirationDate(null); // clear the expiration date
+    setUserId(null);
+    localStorage.removeItem('userData');
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+    }
+  }, [login]);
+
+  return { token, login, logout, userId };
+}
