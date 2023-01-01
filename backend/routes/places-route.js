a4ec32b9a24da7082705506b50578510cec8625c
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-12-31 20:38:26 -0500

    add to upload place image and save it in the backend

--- backend/routes/places-route.js
+++ backend/routes/places-route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 const { getPlaceById, getPlacesByUserId, createPlace, updatePlaceById, deletePlaceById } = require('../controllers/places-controllers');
+const fileUpload = require('../middleware/file-upload'); 
 
 const router = express.Router(); // To register routes - can export this router to app.js
 
@@ -8,6 +9,7 @@ router.get('/:pid', getPlaceById);
 router.get('/user/:uid', getPlacesByUserId);
 router.post(
   '/', 
+  fileUpload.single('image'), // single() is a middleware function that will run before the createPlace function
   [
     check('title').notEmpty(), 
     check('description').isLength({min: 5}),
--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -8,6 +8,7 @@ import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/valida
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 
 import './PlaceForm.css';
 
@@ -27,6 +28,10 @@ const NewPlace = () => {
       address: {
         value: '',
         isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
       }
     },
     false
@@ -41,18 +46,16 @@ const NewPlace = () => {
     event.preventDefault();
     console.log(formState.inputs); // send this to the backend
     try {
+      const formData = new FormData(); // FormData is a built-in browser API
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('creator', auth.userId);
+      formData.append('image', formState.inputs.image.value);
       await sendRequest(
         'http://localhost:5000/api/places', 
         'POST', 
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId
-        }),
-        {
-          'Content-Type': 'application/json'
-        }
+        formData
       );
       history.push('/'); // redirect the user to the root page
     } catch (err) {} // error is handled by the useHttpClient hook
@@ -88,6 +91,7 @@ const NewPlace = () => {
           errorText="Please enter a valid address"
           onInput={inputHandler}
         />
+        <ImageUpload center id="image" onInput={inputHandler} errorText="Please provide an image." />
         <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
       </form>
     </React.Fragment>
